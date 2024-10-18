b5ee9e7763fcc9e3af6794097d735ca0c3a666cc
MirrorX <51252864+Mirror995@users.noreply.github.com>
2024-10-19 03:45:48 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -2,12 +2,7 @@ const express =require('express');
 const axios = require('axios');
 const app = express();
 
-app.use((req, res, next) => {
-  setTimeout(function() {
-    console.log('等了一会');}, 30000);
-  const url = `https://mdict.onrender.com/`;
-  const url2 = `https://spindown-solution-d7wv.onrender.com/`;
-  function reloadWebsite(a) {
+function reloadWebsite(a) {
   axios.get(a)
     .then(response => {
       console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
@@ -16,12 +11,20 @@ app.use((req, res, next) => {
       console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
     });
   }
-  reloadWebsite(url)
+
+app.use((req, res, next) => {
+  setTimeout(function() {
+  console.log('等了一会');}, 30000);
+  const url2 = `https://spindown-solution-d7wv.onrender.com/`;
   reloadWebsite(url2)
   next();
 });
 
 const port = 3000;
 app.listen(port, () => {
+  setTimeout(function() {
+    console.log('等了一会');}, 30000);
   console.log(`服务器正在 http://localhost:${port} 上运行`);
+  const url = `https://mdict.onrender.com/`;
+  reloadWebsite(url)
 });
