e7028d77783dfa207690acf26102792369f63cdd
MirrorX <51252864+Mirror995@users.noreply.github.com>
2024-10-19 02:57:44 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express =require('express');
 const axios = require('axios');
 
-
-const url = `https://mdict.onrender.com/`;
-
-
-
-const interval = 30000;
-
-function reloadWebsite() {
+app.use((req, res, next) => {
+  const url = `https://mdict.onrender.com/`;
+  const interval = 30000;
+  function reloadWebsite() {
   axios.get(url)
     .then(response => {
       console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
@@ -16,9 +12,12 @@ function reloadWebsite() {
     .catch(error => {
       console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
     });
-}
+  }
+  setInterval(reloadWebsite, interval);
+  next();
+});
 
-setInterval(reloadWebsite, interval);
-
-//
-
+const port = 3000;
+app.listen(port, () => {
+  console.log(`服务器正在 http://localhost:${port} 上运行`);
+});
